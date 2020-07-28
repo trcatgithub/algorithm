ece04e911a05344a1887e49d4a546dd34954250d
trcatgithub <andalishi@126.com>
2020-07-28 09:05:10 +0800

    20200728每日一题 104. 二叉树的最大深度（递归dfs）

---
+++ src/main/java/tree/Solution4.java
@@ -0,0 +1,36 @@
+package tree;
+
+//给定一个二叉树，找出其最大深度。
+//
+//二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+//说明: 叶子节点是指没有子节点的节点。
+//
+//示例：
+//给定二叉树 [3,9,20,null,null,15,7]，
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//返回它的最大深度 3 。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+public class Solution4 {
+    // dfs
+    // 0ms/39.7MB
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+    }
+
+    public static void main(String[] args) {
+        Integer[] tree = new Integer[]{3,9,20,null,null,15,7};
+        System.out.println(new Solution4().maxDepth(TreeUtils.generateTree(tree)));
+    }
+}
